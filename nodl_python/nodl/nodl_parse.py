ba0a5ef5bdedaffd9bb942f1f45811afd814243e
Ted Kern <ted.kern@canonical.com>
2020-02-05 19:56:11 +0000

    docstrings and copyright
    Signed-off-by: Ted Kern <ted.kern@canonical.com>

--- nodl_python/nodl/nodl_parse.py
+++ nodl_python/nodl/nodl_parse.py
@@ -10,6 +10,7 @@
 # You should have received a copy of the GNU Limited General Public License along with this program.
 # If not, see <http://www.gnu.org/licenses/>.
 
+
 from pathlib import Path
 from typing import List, Optional
 import warnings
@@ -30,6 +31,8 @@ def str_to_bool(boolean_string: str) -> bool:
 
 
 class InvalidNoDLException(Exception):
+    """Exception class representing most errors in parsing the NoDL tree."""
+
     def __init__(self, message: str, element: Optional[etree._Element] = None) -> None:
         if element is not None:
             super().__init__(f"{element.base}:{element.sourceline}  " + message)
@@ -38,18 +41,23 @@ class InvalidNoDLException(Exception):
 
 
 class UnsupportedInterfaceException(InvalidNoDLException):
+    """Exception thrown when an interface has a future or invalid version."""
+
     def __init__(self, element: etree._Element) -> None:
         super().__init__(
             f"{element.base}:{element.sourceline}  Unsupported interface version: "
-            f"{element.attrib['version']} > {NODL_MAX_SUPPORTED_VERSION}"
+            f"{element.attrib['version']} (must be <={NODL_MAX_SUPPORTED_VERSION})"
         )
 
 
 class NoNodeInterfaceWarning(UserWarning):
+    """Warning raised when not marked as either server/client or pub/sub"""
+
     pass
 
 
 def parse_element_tree(element_tree: etree._ElementTree) -> etree._Element:
+    """Extract an interface element from an ElementTree if present."""
     interface = element_tree.getroot()
     if interface.tag != "interface":
         interface = next(interface.iter("interface"), None)
@@ -59,7 +67,7 @@ def parse_element_tree(element_tree: etree._ElementTree) -> etree._Element:
 
 
 def parse_nodl_file(path: Path) -> List[Node]:
-    """"""
+    """Parse the nodes out of a given NoDL file."""
     full_path = path.resolve()
     element_tree = etree.parse(str(full_path))
     interface = parse_element_tree(element_tree)
@@ -76,7 +84,7 @@ def parse_nodl_file(path: Path) -> List[Node]:
 
 
 def parse_qos(element: Optional[etree._Element]) -> qos.QoSProfile:
-    """"""
+    """Populate a QoS Profile from a qos NoDL element"""
     profile: qos.QoSProfile = qos.QoSProfile(**qos.QoSProfile._QoSProfile__qos_profile_default_dict)
 
     if element is not None:
@@ -116,11 +124,11 @@ def parse_qos(element: Optional[etree._Element]) -> qos.QoSProfile:
 
 
 class Interface_v1:
-    """"""
+    """Parser methods for v1"""
 
     @classmethod
     def parse_action(cls, element: etree._Element) -> Action:
-        """"""
+        """Parse a NoDL action from an xml element"""
         # ETree.Element contains a `get()` feature that can be used to avoid
         # a potential dict creation from accessing `element.attrib`. We don't use
         # this because we want a KeyError if the required fields aren't there.
@@ -144,12 +152,12 @@ class Interface_v1:
 
     @classmethod
     def parse_parameter(cls, element: etree._Element) -> Parameter:
-        """"""
+        """Parse a NoDL parameter from an xml element"""
         return Parameter(name=element.attrib["name"], parameter_type=element.attrib["type"])
 
     @classmethod
     def parse_service(cls, element: etree._Element) -> Service:
-        """"""
+        """Parse a NoDL service from an xml element"""
         attribs = element.attrib
         name = attribs["name"]
         service_type = attribs["type"]
@@ -175,7 +183,7 @@ class Interface_v1:
 
     @classmethod
     def parse_topic(cls, element: etree._Element) -> Topic:
-        """"""
+        """Parse a NoDL topic from an xml element"""
         attribs = element.attrib
         name = attribs["name"]
         message_type = attribs["type"]
@@ -200,13 +208,13 @@ class Interface_v1:
 
     @classmethod
     def parse_nodes(cls, interface) -> List[Node]:
-        """"""
-
+        """Parse the nodes contained in an interface element and return a list"""
         node_elements = [child for child in interface if child.tag == "node"]
         return [cls.parse_node(node) for node in node_elements]
 
     @classmethod
     def parse_node(cls, node: etree._Element) -> Node:
+        """Parse a NoDL node and all the elements it contains from an xml element."""
         name = node.attrib["name"]
 
         actions = []
